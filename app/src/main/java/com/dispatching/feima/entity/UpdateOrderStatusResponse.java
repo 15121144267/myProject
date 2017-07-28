4679cd894582f0bc0eff3c0a37fcf9c042641727
lei.he <lei.he@freemud.cn>
2017-07-28 17:54:19 +0800

    1

---
+++ app/src/main/java/com/dispatching/feima/entity/UpdateOrderStatusResponse.java
@@ -0,0 +1,15 @@
+package com.dispatching.feima.entity;
+
+import java.io.Serializable;
+
+/**
+ * Created by lei.he on 2017/7/28.
+ */
+
+public class UpdateOrderStatusResponse implements Serializable {
+    public Integer StatusCode;
+    public String payAcount;
+    public String payId;
+    public String msg;
+
+}
