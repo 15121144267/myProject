847736ab16679da2ce9172d923174a61107db51d
lei.he <lei.he@freemud.cn>
2017-08-31 17:19:26 +0800

    11

--- app/src/main/java/com/dispatching/feima/view/adapter/GoodsClassifyItemAdapter.java
+++ app/src/main/java/com/dispatching/feima/view/adapter/GoodsClassifyItemAdapter.java
@@ -1,5 +1,10 @@
 package com.dispatching.feima.view.adapter;
 
+import android.support.v7.widget.LinearLayoutCompat;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
 import com.dispatching.feima.R;
 import com.dispatching.feima.entity.SortListResponse;
 import com.example.mylibrary.adapter.BaseQuickAdapter;
@@ -20,6 +25,12 @@ public class GoodsClassifyItemAdapter extends BaseQuickAdapter<SortListResponse.
     @Override
     protected void convert(BaseViewHolder helper, SortListResponse.DataBean.ChildrenBean item) {
         if (item == null) return;
+        LinearLayout linearLayout = helper.getView(R.id.adapter_item_goods_classify_layout);
+        if(helper.getAdapterPosition()%2==0){
+            LinearLayoutCompat.LayoutParams layoutParams = new LinearLayoutCompat.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            layoutParams.setMargins(0,0,8,8);
+            linearLayout.setLayoutParams(layoutParams);
+        }
         helper.setText(R.id.adapter_item_goods_classify, item.resultModel.name);
     }
 }
